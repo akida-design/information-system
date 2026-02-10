78e5aedaee4791dc2d6e00851fd034df051bea30
akida-design <m.akida.info@gmail.com>
2026-02-10 19:53:21 +0300

    created input form and navbar. Missing data fetch, react router,some component content

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import './App.css'
+import Input from './components/Input'
+import Navbar from './components/Navbar'
+
+function App() {
+ 
+
+  return (
+    <>
+      <Navbar />
+      <Input />
+    </>
+  )
+}
+
+export default App
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,15 @@
+function Navbar() {
+    return (
+        <div>
+            <ul className="Navbar">
+                <li>Home</li>
+                <li>About</li>
+                <li>User</li>
+                <li>User Details</li>
+                <li>Add User</li>
+            </ul>
+        </div>
+    )
+};
+
+export default Navbar;
